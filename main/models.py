2f3c0cdd3df938e18f209465d4c2883068b8b104
Shamil <shamilmadi2000@gmail.com>
2022-05-18 20:28:00 +0600

    added accounts

---
+++ main/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Category(models.Model):
+    slug = models.SlugField(max_length=100, pk=True)
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    author = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='posts')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts')
+    title = models.CharField(max_length=300)
+    text = models.TextField()
+    created_at = models.DateTimeField()
+
+
+class PostImage(models.Model):
+    image = models.ImageField(upload_to='posts', blank=True, null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='images')
+
+
+
